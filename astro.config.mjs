a63a0400b9ce2d08f7bab9d71d5f8c22f67466ec
Radosvet Petrov <93radosvet@gmail.com>
2025-04-18 11:18:36 +0100

    removed www

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.radosvet.dev',
+	site: 'https://radosvet.dev',
 	markdown: {
 		syntaxHighlight: false,
 		// Disable syntax built-in syntax hightlighting from astro
